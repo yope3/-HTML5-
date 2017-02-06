0329042e932db7cf915e0c25a42b627e2d88e00a
yope3 <yopef8.dawnofnd@gmail.com>
2017-02-06 14:29:06 +0900

    Add files via upload

---
+++ scripts/myapp.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  //serviceWorkerを登録
+  navigator.serviceWorker.register('/sw.js')
+  .then(function (registration) {
+    // 登録成功の場合
+    console.log('ServiceWorker登録成功');
+  }).catch(function (error) {
+    // 登録失敗の場合
+    console.log('ServiceWorker登録失敗　原因: ', error);
+  });
+}
